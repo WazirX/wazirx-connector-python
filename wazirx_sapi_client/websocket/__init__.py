599d289ed87a9bbf03511d76d18d7385b95901e8
Abhishek Pahwa <apahwa014@gmail.com>
2021-11-22 12:43:01 +0530

    fix: Version check for 3.10 and above (websocket client)

--- wazirx_sapi_client/websocket/__init__.py
+++ wazirx_sapi_client/websocket/__init__.py
@@ -1,6 +1,8 @@
 import sys
 
-if int(sys.version[0]) < 3 or int(sys.version[2]) < 7:
+version = sys.version.split(' ')[0]
+py_mode, py_version, py_sub_version = version.split('.')
+if int(py_mode) < 3 or int(py_version) < 7:
     raise BaseException("Python>=3.7 required")
 
 from .websocket_client import WebsocketClient
