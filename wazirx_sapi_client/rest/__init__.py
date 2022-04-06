78c1225af15528aa0385e290f48f16621a2532a9
Rohit Shrivastava <rohitxsh@gmail.com>
2022-04-07 01:25:01 +0530

    fix: Fix py version check for >= 3.10

--- wazirx_sapi_client/rest/__init__.py
+++ wazirx_sapi_client/rest/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-if int(sys.version[0]) < 3 or int(sys.version[2]) < 7:
+if int(sys.version[0]) < 3 or int(sys.version.split('.')[1]) < 7:
     raise BaseException("Python>=3.7 required")
 
 from wazirx_sapi_client.rest.client import Client
