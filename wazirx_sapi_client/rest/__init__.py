9ebf4c2041ef37e5c41fdafd3f6ff8af79b61d3a
0xcrypto <6683856+0xcrypto@users.noreply.github.com>
2024-02-10 14:24:35 +0530

    Fixed python version check for 3.11

--- wazirx_sapi_client/rest/__init__.py
+++ wazirx_sapi_client/rest/__init__.py
@@ -1,6 +1,8 @@
 import sys
 
-if int(sys.version[0]) < 3 or int(sys.version[2]) < 7:
+python_version = sys.version.split(".")
+
+if int(python_version[0]) < 3 or int(python_version[1]) < 7:
     raise BaseException("Python>=3.7 required")
 
 from wazirx_sapi_client.rest.client import Client
